0e1924c9e1580ef50c40a3ac9b0d2273733042b9
djsterster <57191607+djsterster@users.noreply.github.com>
2020-04-12 09:54:32 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,22 @@
+'use strict';
+// Import the discord.js module
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.content === 'I challenge you to a duel') {
+    message.channel.send('Are you sure you want to do this?');
+    client.on('message', message=> {
+      if(message.content ==="Yes"){
+        message.channel.send('Ok');
+        new Discord.DMChannel(client, data);
+      }
+    })
+  }
+});
+
+
+client.login('Njk3MTEwODA5NjgzMTY1Mjk3.Xo83PA.H0s-DQWTxJQP7IBAnd4_QQ--pTM');
